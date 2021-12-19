0720f31d03945195d793f4200687bf9dae926498
JuanSaldana1 <saldana.juan.2002@gmail.com>
2021-12-19 13:32:12 +0100

    Managing props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 
-function HelloWorld() {
+function HelloWorld(props) {
+    console.log(props);
     return (
-        <div id="hello">Hello World!</div>
+        <div id="hello">
+            <h3>{props.subtitle}</h3>
+            <p>{props.myText}</p>
+        </div>
     );
 }
 
 function App() {
     return (
-        <div>This is my component: <HelloWorld/></div>
+        <div>This is my component:
+            <HelloWorld myText={"HOllllaaaa"} subtitle={"Lorem Ipsun"}/>
+            <HelloWorld myText={"Hello Juan"}/>
+            <HelloWorld myText={"Hello World!"}/>
+        </div>
     );
 }
 
