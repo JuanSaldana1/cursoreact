b4303b6a81bcf22addbda6a818a9edef7c934564
JuanSaldana1 <saldana.juan.2002@gmail.com>
2021-12-19 13:11:15 +0100

    New Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+function HelloWorld() {
+    return (
+        <div>Hello World!</div>
+    );
+}
+
 function App() {
-  return (
-    <div>Hello World!</div>
-  );
+    return (
+        <div>This is my component: <HelloWorld/></div>
+    );
 }
 
 export default App;
